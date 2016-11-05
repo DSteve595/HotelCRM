644838b7d7de15855966a58ccc9978ba4eb81fc5
Bradley <bradleyy1012@gmail.com>
2016-11-05 17:19:17 -0400

    first commit

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Welcome page for hotel employees and owners to see
+Route::get('/', 'HotelAgentController@index');
