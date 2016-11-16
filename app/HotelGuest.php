635696d60d6106145fbdb020cbf0267cac56bd97
Bradley <bradleyy1012@gmail.com>
2016-11-15 19:48:36 -0500

    Updates to data structure relationships

--- app/HotelGuest.php
+++ app/HotelGuest.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class HotelGuest extends Model
 {
-    //
+    protected $fillable = [
+
+    ];
+
+    
+    /*
+     * Relationships go below
+     */
+
+    public function user()
+    {
+        $this->belongsTo('App\User');
+    }
 }
