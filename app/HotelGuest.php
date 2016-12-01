3a03923a9b82ccf095e92427a6363a6bcdb24ea3
Steven Schoen <steven@stevenschoen.com>
2016-11-30 21:19:05 -0500

    Fix another relationship issue

--- app/HotelGuest.php
+++ app/HotelGuest.php
@@ -26,7 +26,7 @@ class HotelGuest extends Model
 
     public function user()
     {
-        return $this->belongsTo('App\User', 'id', 'user_id');
+        return $this->belongsTo('App\User', 'user_id', 'id');
     }
 
 }
