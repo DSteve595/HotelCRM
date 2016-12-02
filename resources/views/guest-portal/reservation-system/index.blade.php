0564af8f7f02b8d8a101fa2e278551d3aeb3a553
Bradley <bradleyy1012@gmail.com>
2016-12-01 20:10:37 -0500

    fixed missing thing whatever

--- resources/views/guest-portal/reservation-system/index.blade.php
+++ resources/views/guest-portal/reservation-system/index.blade.php
@@ -2,3 +2,19 @@
 @section('theme')
     dark
 @endsection
+
+
+@section('content')
+
+    <div class="row">
+        <div class="col-xs-12">
+
+            <h3 class="page-header">
+                Reservations <small>Ready to make a reservation?</small>
+            </h3>
+
+        </div>
+    </div> <!-- / .row -->
+
+    @include('guest-portal.reservation-system.search')
+@endsection
